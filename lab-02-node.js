feb2a146f8c30f2dc43010fdd0dcdb4964586176
sierrabakerr <102761255+sierrabakerr@users.noreply.github.com>
2022-06-02 20:47:06 -0700

    Add files via upload

---
+++ lab-02-node.js
@@ -0,0 +1,15 @@
+/*
+    CIT 281 Lab 2
+    Name: Sierra Baker
+*/
+function square(num) {
+    return num*num;
+}
+console.log('Square operations:')
+for (let i = 2; i <= 10; i+=2) {
+    console.log(`Square of ${i} is ${square(i)}`);
+}
+
+console.log('Square of 20 is ${square(10)}');
+
+
